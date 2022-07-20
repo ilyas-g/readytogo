e56be2d5aa38dfa24edfe23d72c279a9fb9ef0f6
Roddoark <ilyas150691@hotmail.fr>
2022-07-20 16:59:15 +0200

    Adding buttons and render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ready To Go</h1>
       </header>
+      <hr />
+      <div>
+
+        <button
+          className={`${switch1 === true ? 'active' : ''}`}
+          onClick={() => {
+            setSwitch1(true);
+          }}
+        >On</button>
+
+        <button
+          className={`${switch1 === false ? 'active' : ''}`}
+          onClick={() => {
+            setSwitch1(false);
+          }}>Off</button>
+      </div>
+      <div>
+        <button
+          className={`${switch2 === true ? 'active' : ''}`}
+          onClick={() => {
+            setSwitch2(true);
+          }}>On</button>
+        <button
+          className={`${switch2 === false ? 'active' : ''}`}
+          onClick={() => {
+            setSwitch2(false);
+          }}>Off</button>
+      </div>
+      <div>
+        <button
+          className={`${switch3 === true ? 'active' : ''}`}
+          onClick={() => {
+            setSwitch3(true);
+          }}>On</button>
+        <button
+          className={`${switch3 === false ? 'active' : ''}`}
+          onClick={() => {
+            setSwitch3(false);
+          }}>Off</button>
+      </div>
+
+      <h2>
+        {
+          switch1 === true &&
+            switch2 === true &&
+            switch3 === true ? 'Go !' : 'No way !'
+        }
+      </h2>
     </div>
   );
 }
