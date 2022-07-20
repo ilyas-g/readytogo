e33de070c9080d2721aba18c9fe4afc9398219e5
Roddoark <ilyas150691@hotmail.fr>
2022-07-20 17:02:18 +0200

    Re-order of the code

--- src/App.js
+++ src/App.js
@@ -13,43 +13,41 @@ function App() {
       </header>
       <hr />
       <div>
-
+        <button
+          className={`${switch1 === false ? 'active' : ''}`}
+          onClick={() => {
+            setSwitch1(false);
+          }}>Off</button>
         <button
           className={`${switch1 === true ? 'active' : ''}`}
           onClick={() => {
             setSwitch1(true);
           }}
         >On</button>
-
-        <button
-          className={`${switch1 === false ? 'active' : ''}`}
-          onClick={() => {
-            setSwitch1(false);
-          }}>Off</button>
       </div>
       <div>
-        <button
-          className={`${switch2 === true ? 'active' : ''}`}
-          onClick={() => {
-            setSwitch2(true);
-          }}>On</button>
         <button
           className={`${switch2 === false ? 'active' : ''}`}
           onClick={() => {
             setSwitch2(false);
           }}>Off</button>
+        <button
+          className={`${switch2 === true ? 'active' : ''}`}
+          onClick={() => {
+            setSwitch2(true);
+          }}>On</button>
       </div>
       <div>
-        <button
-          className={`${switch3 === true ? 'active' : ''}`}
-          onClick={() => {
-            setSwitch3(true);
-          }}>On</button>
         <button
           className={`${switch3 === false ? 'active' : ''}`}
           onClick={() => {
             setSwitch3(false);
           }}>Off</button>
+        <button
+          className={`${switch3 === true ? 'active' : ''}`}
+          onClick={() => {
+            setSwitch3(true);
+          }}>On</button>
       </div>
 
       <h2>
